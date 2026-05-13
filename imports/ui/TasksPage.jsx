a5d40f13d061a3445cdb4581f838d5974c214cbc
Germano <germanomarquescfagundes@gmail.com>
2026-05-13 11:08:45 -0300

    fix: proper authentication on reload of /tasks

--- imports/ui/TasksPage.jsx
+++ imports/ui/TasksPage.jsx
@@ -3,20 +3,21 @@ import { useTracker } from "meteor/react-meteor-data";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Fragment } from "react";
 import { ToDoList } from "./ToDoList";
+import { Meteor } from "meteor/meteor";
 
 export const TasksPage = () => {
-  const user = useTracker(() => Meteor.user());
+  const { user, isLoggingIn } = useTracker(() => ({
+    user: Meteor.user(),
+    isLoggingIn: Meteor.loggingIn(),
+  }));
+
+  if (isLoggingIn) return <div>Carregando sessão...</div>;
+  if (!user) return <Navigate to="/" replace />;
 
   return (
     <div>
       <CssBaseline />
-      {user ? (
-        <Fragment>
-          <ToDoList />
-        </Fragment>
-      ) : (
-        <Navigate to="/" replace />
-      )}
+      <ToDoList />
     </div>
   );
 };
